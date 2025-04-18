edc3ef2f637896ab8542ff7320bf09fc7c02ed89
sarabjitsinha <sinhasarabjit@gmail.com>
2025-04-18 15:14:48 +0530

    Fixed POST issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Upload from './pages/Upload';
+import Login from './withAuth/Login';
+import Signup from './withAuth/Signup';
+import Dashboard from './withAuth/Dashboard';
+import Upload from './withAuth/Upload';
 import { AuthProvider } from './contexts/Authcontext';
-import CreatePath from './pages/Createpath';
-import Headers from './pages/Headers';
+import Headers from './components/Headers';
 
 function App() {
 
-  
-  return (
+    return (
     <AuthProvider>
-          <Router>
+     <Router>
       <Headers/>
           <Routes>
-          <Route path="/" element={[<Upload key={0}/>,<Dashboard key={1}/>]} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path='/createpath' element={<CreatePath/>}/>
-        </Routes>
+            <Route path="/" element={[<Upload key={0}/>,<Dashboard key={1}/>]} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/upload" element={<Upload />} />
+          </Routes>
       </Router>
     </AuthProvider>
   );
